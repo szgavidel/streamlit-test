80780471fb6e332bc4187cb9e39310659c80f528
szgavidel <52538461+szgavidel@users.noreply.github.com>
2024-06-20 21:27:33 -0400

    comit 5

--- test.py
+++ test.py
@@ -8,9 +8,14 @@ st.radio("st.radio",["A","B","C"])
 
 
 st.checkbox("st.checkbox")
-x = st.multiselect("multiselect",['A','B','C']) 
-st.write(x)
-st.slider("slider",min_value=0., max_value=1., step=0.05)
+st.multiselect("multiselect",['A','B','C']) 
+
+global x
+y = st.slider("slider",min_value=0., max_value=1., step=0.05)
+def f(x):
+    return x+1
+st.write(f(y))
+
 st.number_input("Number input field")
 st.date_input("Date input field")
 st.time_input("Time input field")
