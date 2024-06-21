f9e48dd87654ab5a9e37e81ad4e15114b0e03846
szgavidel <52538461+szgavidel@users.noreply.github.com>
2024-06-20 21:13:44 -0400

    comit 4

--- test.py
+++ test.py
@@ -4,12 +4,12 @@ import pandas as pd
 import numpy as np 
 import streamlit as st 
 
-x = st.radio("st.radio",["A","B","C"])
-if x=='A':
-    print("you chose A")
+st.radio("st.radio",["A","B","C"])
+
 
 st.checkbox("st.checkbox")
-st.multiselect("multiselect",['A','B','C']) 
+x = st.multiselect("multiselect",['A','B','C']) 
+st.write(x)
 st.slider("slider",min_value=0., max_value=1., step=0.05)
 st.number_input("Number input field")
 st.date_input("Date input field")
